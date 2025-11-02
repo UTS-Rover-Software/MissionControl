d9eb1cf3fccd4244f993971938def602d5a78043
Jaden Gambrill <jadengambrill@gmail.com>
2025-11-02 19:24:56 +1100

    Initial Mission Control front-end setup

---
+++ front-end/src/App.tsx
@@ -0,0 +1,19 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="container">
+      <img
+        src="/rover_logo_redbg.svg"
+        alt="Rover Logo"
+        className="logo"
+      />
+      <h1 className="title">Welcome to Mission Control</h1>
+      <p className="subtitle">
+        Front-End Scaffold for Mission Control has begun!
+      </p>
+    </div>
+  );
+}
+
+export default App;
